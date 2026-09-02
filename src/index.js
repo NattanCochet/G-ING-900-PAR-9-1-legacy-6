29fb0c00401fa1458c1c9b8a8a7472912fb7178c
NattanCochet <nattan.cochet@epitech.eu>
2026-09-02 15:16:04 +0200

    [ADD] Dockerfile, Docker-compose, github actions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
 app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/front/static'));
 
 app.get('/items', getItems);
 app.post('/items', addItem);
@@ -15,7 +15,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
+    app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
