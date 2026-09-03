8b6403afdcc6b76a313cbd77213b419a20e22181
NattanCochet <nattan.cochet@epitech.eu>
2026-09-03 14:08:37 +0200

    [FEAT]: Added scalar backend documentation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const { apiReference } = require('@scalar/express-api-reference');
 
 app.use(express.json());
 app.use(express.static(__dirname + '/front/static'));
@@ -13,6 +14,9 @@ app.get('/items', getItems);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
+
+app.get('/openapi.json', (req, res) => res.sendFile(__dirname + '/openapi.json'));
+app.use('/reference', apiReference({ url: '/openapi.json' }));
 
 db.init().then(() => {
     app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`));
